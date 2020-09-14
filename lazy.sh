4d44e736e5df3298ed21c969fd5c06353a7979e8
Stanislav Stoianov <stanis.stoyanov@outlook.com>
2020-09-14 15:54:22 +0300

    Initial commit && added second homework

---
+++ lazy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# https://habr.com/ru/company/ruvds/blog/325522/
+
+select=$1
+move="move"
+delete="delete"
+copy="copy"
+create="create"
+
+echo "$select was selected"
+
+if [ $select == $move ]; then
+    echo "which file?"
+    read filename
+    echo "where to?"
+    read path
+    echo mv $filename $path
+    exit
+elif [ $select == $delete ]; then
+    echo "which file?"
+    read filename
+    rm -rf $filename
+    exit
+elif [ $select == $copy ]; then
+    echo "which file?"
+    read filename
+    echo "where to?"
+    read filename2
+    echo cp $filename $filename2
+    exit
+elif [ $select == $create ]; then
+    echo "which file?"
+    read filename
+    touch $filename
+    exit
+else
+    echo "Wrong key"
+    exit
+fi
